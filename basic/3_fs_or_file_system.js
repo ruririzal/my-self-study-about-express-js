636c767f160242939d9ee883b697c1a38b6847d5
ruririzal <hash702@gmail.com>
2022-05-18 23:18:35 +0700

    basic 3 fs

--- basic/3_fs_or_file_system.js
+++ basic/3_fs_or_file_system.js
@@ -9,7 +9,7 @@ fs.mkdir(path.join(__dirname, 'test'), {recursive : false}, err => {
     // if(err) throw err;
     if(err) console.log(err);
 
-    console.log('folder created');
+    console.log('-folder created');
 });
 
 // asyncronous create and write to file (overwrite)
@@ -17,7 +17,7 @@ fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), data = 'Hello world!', e
     // if(err) throw err;
     if(err) console.log(err);
 
-    console.log('file writed');
+    console.log('-file writed');
 });
 
 // asyncronous create, write and append data to file 
@@ -25,5 +25,22 @@ fs.appendFile(path.join(__dirname, 'test', 'opo.txt'), data = '\napped', (err, d
     // if(err) throw err;
     if(err) console.log(err);
 
-    console.log('file append');
+    console.log('-file append');
 });
+
+// asyncronous read data on file 
+// utf-8 digunakan agar data berisi data file, bukannya buffer data
+fs.readFile(path.join(__dirname, 'test', 'opo.txt'), 'utf-8', (err, data) => {
+    // if(err) throw err;
+    if(err) console.log(err);
+
+    console.log(data); 
+});
+
+// asyncronous rename file 
+fs.rename(path.join(__dirname, 'test', 'opo.txt'), path.join(__dirname, 'test', 'opo_ren.txt'), err => {
+    // if(err) throw err;
+    if(err) console.log(err);
+
+    console.log('-file renamed'); 
+})
