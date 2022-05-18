f7a1212da49583c6e93f1e38330543fc0e0e08b4
ruririzal <hash702@gmail.com>
2022-05-18 22:48:06 +0700

    basic 1

---
+++ basic/1-module-wrapper-function.js
@@ -0,0 +1,16 @@
+// run this file : node basic/1-module-wrapper-function
+
+// variabel : exports, require, module, __filename, __dirname
+// akan selalu berjalan dahulu difile ini ketika file di running
+
+
+// Module wrapper function
+(function(exports, require, module, __filename, __dirname){
+
+    
+})
+console.log(exports);
+console.log(require);
+console.log(module);
+console.log(__filename);
+console.log(__dirname);
